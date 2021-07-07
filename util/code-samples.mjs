678462eabb8c907ef65866ae855c81de77f622c6
Benjamin Jentsch <benjamin.jentsch@gutefrage.net>
2021-07-07 11:19:30 +0200

    GD-3016, GD-3017: add formatted HTML snippet to code samples

--- util/code-samples.mjs
+++ util/code-samples.mjs
@@ -1,13 +1,33 @@
+import prettier from "https://cdn.jsdelivr.net/npm/prettier@2.3.2/esm/standalone.mjs";
+import parserHtml from "https://cdn.jsdelivr.net/npm/prettier@2.3.2/esm/parser-html.mjs";
+
+/**
+ * @param {{js: string, css: string, html: string}} sources
+ */
 export const includeCodeSamples = (sources) => {
   Promise.all(
-    sources.map((source) => {
-      const keys = Object.keys(source);
-      const codeElement = document.querySelector(`[data-ref=${keys[0]}]`);
+    Object.keys(sources).map(
+      /**
+       * @param {"js" | "css" | "html"} type
+       */
+      (type) => {
+        const codeElement = document.querySelector(`[data-ref=${type}]`);
 
-      return window
-        .fetch(source[keys[0]], { method: "GET" })
-        .then((result) => result.text())
-        .then((text) => codeElement.appendChild(document.createTextNode(text)));
-    })
+        return (
+          type === "html"
+            ? Promise.resolve(
+                prettier.format(
+                  document.querySelector(sources.html)?.outerHTML,
+                  { parser: "html", plugins: [parserHtml] }
+                )
+              )
+            : window
+                .fetch(sources[type], { method: "GET" })
+                .then((result) => result.text())
+        ).then((text) =>
+          codeElement.appendChild(document.createTextNode(text))
+        );
+      }
+    )
   ).then(() => hljs.highlightAll());
 };
